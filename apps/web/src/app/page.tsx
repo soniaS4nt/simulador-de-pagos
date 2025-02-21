9c388ff28e9124458d1b3b48f48361010ea62d67
“sonia” <soniachaparro21@gmail.com>
2025-02-21 01:03:57 -0300

    Add initial web application setup with global styles, fonts, and payment receipt functionality

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import PaymentForm from '@/components/PaymentForm'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-2 p-sm-24">
+      <PaymentForm />
+    </main>
+  )
+}

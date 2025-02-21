303bbbdc6dc261bb8b0a7bea898045141816be6f
“sonia” <soniachaparro21@gmail.com>
2025-02-21 00:07:27 -0300

    Add initial project structure with configuration files, global styles, and assets

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PaymentForm from '@/components/PaymentForm'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-2 p-sm-24">
-      <PaymentForm />
-    </main>
-  )
-}
